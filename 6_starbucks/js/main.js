244120df5181452113aa52788b716798a8a91d08
shindonghyun <donghyun5349@naver.com>
2021-04-15 23:40:12 +0900

    6_startbucks

--- 6_starbucks/js/main.js
+++ 6_starbucks/js/main.js
@@ -16,3 +16,62 @@ searchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
 });
+
+const badgeEl = document.querySelector('.badges')
+
+window.addEventListener('scroll', _.throttle(function () {
+  console.log(window.scrollY)
+  if (window.scrollY > 500) {
+    // 배지 숨기기
+    // gsap.to(요소, 지속시간, 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+    
+  } else {
+    // 배지 보이기
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+    
+  }
+}, 300));
+
+// _.throttle(함수, 시간) 
+
+const fadeEls = document.querySelectorAll('.visual .fade-in')
+fadeEls.forEach(function (fadeEl, index) {
+  // gsap.to(요소, 지속시간, 옵션);
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7,
+    opacity: 1
+  })
+});
+
+// new -> 자바스크립트 생성자
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical',
+  autoplay: true,
+  loop: true,
+});
+
+new Swiper('.promotion .swiper-container', {
+  slidersPerView: 3, // 한번에 보여줄 슬라드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데
+  loop: true,
+  // autoplay: {
+  //   delay: 500
+  // }
+  pagination: {
+    el: '.promotion swiper-pagination', // 페이지 번호 요소 선택자
+    clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+});
